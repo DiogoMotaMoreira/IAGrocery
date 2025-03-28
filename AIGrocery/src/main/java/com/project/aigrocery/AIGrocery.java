9fe816cfb9d4094248ebceee2512b6b49a6d4bcb
Diogo Moreira <87901322+DiogoMotaMoreira@users.noreply.github.com>
2025-03-28 22:24:54 +0000

    Add files via upload

---
+++ AIGrocery/src/main/java/com/project/aigrocery/AIGrocery.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.project.aigrocery;
+
+/**
+ *
+ * @author diogo
+ */
+public class AIGrocery {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
