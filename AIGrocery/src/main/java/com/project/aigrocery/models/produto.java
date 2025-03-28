caad73187aa88ab8103e593c63f0fa3d8f93d2e3
a107289 <a107289@alunos.uminho.pt>
2025-03-28 22:35:55 +0000

    prod var

---
+++ AIGrocery/src/main/java/com/project/aigrocery/models/produto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.aigrocery.models;
+
+/**
+ *
+ * @author diogo
+ */
+public class produto{
+    private String categoria;
+    private String nome;
+    private String marca;
+    private String id_prod;
+    private double preco;
+    private int quant_em_arm;
+    private int tier_saudavel; 
+    private int tier_dieta;
+    private int tier_familiar;
+    private int tier_marca;
+    private int tier_sustentavel;
+}
