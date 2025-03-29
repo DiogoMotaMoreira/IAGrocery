ff4f97669a2bc72b466f18ead813896b821ca9e6
a106875 <a106875@alunos.uminho.pt>
2025-03-29 01:33:36 +0000

    uuu

--- AIGrocery/src/main/java/com/project/aigrocery/models/historico.java
+++ AIGrocery/src/main/java/com/project/aigrocery/models/historico.java
@@ -1,5 +1,28 @@
 package com.project.aigrocery.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class historico {
-    
+    private List<produto> produtos;
+
+    public historico() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public List<produto> getProdutos() {
+        return produtos;
+    }
+
+    public void adicionarProduto(produto p) {
+        this.produtos.add(p);
+    }
+
+    public void removerProduto(produto p) {
+        this.produtos.remove(p);
+    }
+
+    public void limparHistorico() {
+        this.produtos.clear();
+    }
 }
